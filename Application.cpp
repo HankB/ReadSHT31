49a67ef6115be7df7175a00974a38e124be65af4
Hank Barta <hbarta@gmail.com>
2026-03-07 19:30:21 -0600

    reformat per VS Code style

--- Application.cpp
+++ Application.cpp
@@ -17,104 +17,133 @@
 //
 #include "Application.hpp"
 
-
-namespace lr {
-
-
-void Application::showHelp()
+namespace lr
 {
-    std::cerr << "Usage: read_sht31 [arguments]" << std::endl;
-    std::cerr << " -h|--help   Display this help." << std::endl;
-    std::cerr << " -s          Get the sensor status." << std::endl;
-    std::cerr << " -t1 -t0     Enable/disable heater." << std::endl;
-    std::cerr << " -a0 -a1     Select the chip address. 0 is the default." << std::endl;
-    std::cerr << " -b0 -b1     Select the bus. 1 is the default." << std::endl;
-    std::cerr << " -d          Show debugging messages." << std::endl;
-}
 
+    void Application::showHelp()
+    {
+        std::cerr << "Usage: read_sht31 [arguments]" << std::endl;
+        std::cerr << " -h|--help   Display this help." << std::endl;
+        std::cerr << " -s          Get the sensor status." << std::endl;
+        std::cerr << " -t1 -t0     Enable/disable heater." << std::endl;
+        std::cerr << " -a0 -a1     Select the chip address. 0 is the default." << std::endl;
+        std::cerr << " -b0 -b1     Select the bus. 1 is the default." << std::endl;
+        std::cerr << " -d          Show debugging messages." << std::endl;
+    }
 
-bool Application::parseCommandLine(int argc, char **argv)
-{
-    for (int i = 1; i < argc; ++i) {
-        const auto arg = std::string(argv[i]);
-        if (arg == "-h" || arg == "--help") {
-            showHelp();
-            return false;
-        } else if (arg == "-d") {
-            _debuggingEnabled = true;
-        } else if (arg == "-s") {
-            _action = Action::ReadStatus;
-        } else if (arg == "-t1") {
-            _action = Action::HeaterEnable;
-        } else if (arg == "-t0") {
-            _action = Action::HeaterDisable;
-        } else if (arg == "-a0") {
-            _chipAddress = lr::SHT31::Address0;
-        } else if (arg == "-a1") {
-            _chipAddress = lr::SHT31::Address1;
-        } else if (arg == "-b0") {
-            _bus = 0;
-        } else if (arg == "-b1") {
-            _bus = 1;
-        } else {
-            std::cerr << "Unknown argument \"" << arg << "\"." << std::endl;
-            showHelp();
-            return false;
+    bool Application::parseCommandLine(int argc, char **argv)
+    {
+        for (int i = 1; i < argc; ++i)
+        {
+            const auto arg = std::string(argv[i]);
+            if (arg == "-h" || arg == "--help")
+            {
+                showHelp();
+                return false;
+            }
+            else if (arg == "-d")
+            {
+                _debuggingEnabled = true;
+            }
+            else if (arg == "-s")
+            {
+                _action = Action::ReadStatus;
+            }
+            else if (arg == "-t1")
+            {
+                _action = Action::HeaterEnable;
+            }
+            else if (arg == "-t0")
+            {
+                _action = Action::HeaterDisable;
+            }
+            else if (arg == "-a0")
+            {
+                _chipAddress = lr::SHT31::Address0;
+            }
+            else if (arg == "-a1")
+            {
+                _chipAddress = lr::SHT31::Address1;
+            }
+            else if (arg == "-b0")
+            {
+                _bus = 0;
+            }
+            else if (arg == "-b1")
+            {
+                _bus = 1;
+            }
+            else
+            {
+                std::cerr << "Unknown argument \"" << arg << "\"." << std::endl;
+                showHelp();
+                return false;
+            }
         }
+        return true;
     }
-    return true;
-}
 
-
-int Application::run(int argc, char **argv)
-{
-    // Initialize and open the bus.
-    if (!parseCommandLine(argc, argv)) {
-        return 1;
-    }
-    lr::SHT31 shtAccess(_chipAddress, _bus, _debuggingEnabled);
-    if (hasError(shtAccess.openBus())) {
-        return 1;
-    }
-    if (_action == Action::ReadValues) {
-        const auto readResult = shtAccess.readValues();
-        if (hasError(readResult)) {
+    int Application::run(int argc, char **argv)
+    {
+        // Initialize and open the bus.
+        if (!parseCommandLine(argc, argv))
+        {
             return 1;
         }
-        // Output the values as JSON
-        const auto [temperature, humidity] = readResult.getValue();
-        std::cout << "{ \"temperature_celsius\": " << temperature << ", \"relative_humidity\": " << humidity << " }" << std::endl;
-    } else if (_action == Action::ReadStatus) {
-        const auto readResult = shtAccess.readStatus();
-        if (hasError(readResult)) {
+        lr::SHT31 shtAccess(_chipAddress, _bus, _debuggingEnabled);
+        if (hasError(shtAccess.openBus()))
+        {
             return 1;
         }
-        // Output the values as JSON
-        std::cout << "{ \"status\": " << readResult.getValue() << " }" << std::endl;
-    } else if (_action == Action::HeaterEnable) {
-        const auto readResult = shtAccess.controlHeater(true);
-        if (hasError(readResult)) {
-            return 1;
+        if (_action == Action::ReadValues)
+        {
+            const auto readResult = shtAccess.readValues();
+            if (hasError(readResult))
+            {
+                return 1;
+            }
+            // Output the values as JSON
+            const auto [temperature, humidity] = readResult.getValue();
+            std::cout << "{ \"temperature_celsius\": " << temperature << ", \"relative_humidity\": " << humidity << " }" << std::endl;
         }
-        // Output the values as JSON
-        std::cout << R"({ "status": "heater_enabled" })" << std::endl;
-    } else if (_action == Action::HeaterDisable) {
-        const auto readResult = shtAccess.controlHeater(false);
-        if (hasError(readResult)) {
-            return 1;
+        else if (_action == Action::ReadStatus)
+        {
+            const auto readResult = shtAccess.readStatus();
+            if (hasError(readResult))
+            {
+                return 1;
+            }
+            // Output the values as JSON
+            std::cout << "{ \"status\": " << readResult.getValue() << " }" << std::endl;
         }
-        // Output the values as JSON
-        std::cout << R"({ "status": "heater_disabled" })" << std::endl;
+        else if (_action == Action::HeaterEnable)
+        {
+            const auto readResult = shtAccess.controlHeater(true);
+            if (hasError(readResult))
+            {
+                return 1;
+            }
+            // Output the values as JSON
+            std::cout << R"({ "status": "heater_enabled" })" << std::endl;
+        }
+        else if (_action == Action::HeaterDisable)
+        {
+            const auto readResult = shtAccess.controlHeater(false);
+            if (hasError(readResult))
+            {
+                return 1;
+            }
+            // Output the values as JSON
+            std::cout << R"({ "status": "heater_disabled" })" << std::endl;
+        }
+        // Close the bus.
+        shtAccess.closeBus();
+        // Success
+        if (_debuggingEnabled)
+        {
+            std::cout << "# Success." << std::endl;
+        }
+        return 0;
     }
-    // Close the bus.
-    shtAccess.closeBus();
-    // Success
-    if (_debuggingEnabled) {
-        std::cout << "# Success." << std::endl;
-    }
-    return 0;
-}
-
 
 }
-
